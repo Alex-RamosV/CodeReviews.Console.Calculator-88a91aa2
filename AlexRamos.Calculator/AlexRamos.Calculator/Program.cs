bf291a3834d03880bff4f04c17c01c06ea4bdf91
AlexRamos_ <arv_lad@hotmail.com>
2026-03-28 16:19:45 -0600

    Challenge Part 01 - Update
    - Amount of times
    - Extra Calculations

--- AlexRamos.Calculator/AlexRamos.Calculator/Program.cs
+++ AlexRamos.Calculator/AlexRamos.Calculator/Program.cs
@@ -4,97 +4,235 @@ using CalculatorLibrary;
 /// <summary>
 /// This class manages the program's execution flow.
 /// </summary>
-namespace CalculatorLibrary
+namespace Program
 {
     class Program
     {
         static void Main(string[] args)
         {
-            bool endApp = false;
-            // Display title as the C# console calculator app.
-            Console.WriteLine("Console Calculator in C#\r");
-            Console.WriteLine("------------------------\n");
+            bool isEndApp = false;
+            int numberOfOperations = 0;
 
             Calculator calculator = new();
 
-            while (!endApp)
+            while (!isEndApp)
             {
-                // Declare variables and set to empty.
-                // Use Nullable types (with ?) to match type of System.Console.ReadLine
-                string? numInput1 = "";
-                string? numInput2 = "";
-                double result = 0;
+                Console.WriteLine("Console Calculator in C# - Main Menu\r");
+                Console.WriteLine("------------------------------------\n");
 
-                // Ask the user to type the first number.
-                Console.Write("Type a number, and then press Enter: ");
-                numInput1 = Console.ReadLine();
+                Console.WriteLine("Choose an math category from the following list:\n");
 
-                double cleanNum1 = 0;
-                while (!double.TryParse(numInput1, out cleanNum1))
+                Console.WriteLine("\t1 - Arithmetic Operations");
+                Console.WriteLine("\t2 - Exponents and Radicals");
+                Console.WriteLine("\t3 - Trigonometric Functions");
+
+                Console.WriteLine($"\nNumber of times calculator was used: {numberOfOperations}");
+
+                Console.Write("\nWhat's your option? ");
+
+                string? mathOperation = Console.ReadLine();
+
+                if (mathOperation == null || !Regex.IsMatch(mathOperation.Trim(), "^[1-3]$"))
                 {
-                    Console.Write("This is not valid input. Please enter a numeric value: ");
-                    numInput1 = Console.ReadLine();
-                }
-
-                // Ask the user to type the second number.
-                Console.Write("Type another number, and then press Enter: ");
-                numInput2 = Console.ReadLine();
-
-                double cleanNum2 = 0;
-                while (!double.TryParse(numInput2, out cleanNum2))
-                {
-                    Console.Write("This is not valid input. Please enter a numeric value: ");
-                    numInput2 = Console.ReadLine();
-                }
-
-                // Ask the user to choose an operator.
-                Console.WriteLine("Choose an operator from the following list:");
-                Console.WriteLine("\ta - Add");
-                Console.WriteLine("\ts - Subtract");
-                Console.WriteLine("\tm - Multiply");
-                Console.WriteLine("\td - Divide");
-                Console.Write("Your option? ");
-
-                string? operation = Console.ReadLine();
-
-                // Validate input is not null, and matches the pattern
-                if (operation == null || !Regex.IsMatch(operation, "[a|s|m|d]"))
-                {
-                    Console.WriteLine("Error: Unrecognized input.");
+                    Console.WriteLine("\nError: Unrecognized math category input.");
                 }
                 else
                 {
-                    try
+                    switch (mathOperation)
                     {
-                        result = calculator.DoOperation(cleanNum1, cleanNum2, operation);
-                        if (double.IsNaN(result))
-                        {
-                            Console.WriteLine("This operation will result in a mathematical error.\n");
-                        }
-                        else
-                        {
-                            Console.WriteLine("Your result: {0:0.##}\n", result);
-                        }
-                    }
-                    catch (Exception e)
-                    {
-                        Console.WriteLine("Oh no! An exception occurred trying to do the math.\n - Details: " + e.Message);
+                        case "1":
+                            Console.Clear();
+
+                            Console.WriteLine("Console Calculator in C# - Basic Arithmetic Operations\r");
+                            Console.WriteLine("------------------------------------------------------\n");
+
+                            Console.WriteLine("Choose an math operation from the following list:\n");
+
+                            Console.WriteLine("\t1 - Add");
+                            Console.WriteLine("\t2 - Subtract");
+                            Console.WriteLine("\t3 - Multiply");
+                            Console.WriteLine("\t4 - Divide");
+
+                            Console.WriteLine($"\nNumber of times calculator was used: {numberOfOperations}");
+
+                            Console.Write("\nWhat's your option? ");
+
+                            mathOperation = Console.ReadLine();
+
+                            if (mathOperation == null || !Regex.IsMatch(mathOperation.Trim(), "^[1-4]$"))
+                            {
+                                Console.WriteLine("\nError: Unrecognized input.");
+                                break;
+                            }
+                            else
+                            {
+                                double cleanNum1 = GetValue();
+                                double cleanNum2 = GetValue();
+
+                                double result = 0;
+
+                                numberOfOperations++;
+
+                                try
+                                {
+                                    result = calculator.BasicArithmeticOperations(cleanNum1, cleanNum2, mathOperation);
+                                    if (double.IsNaN(result))
+                                    {
+                                        Console.WriteLine("This operation will result in a mathematical error.\n");
+                                    }
+                                    else
+                                    {
+                                        Console.WriteLine("\nYour result: {0:0.##}", result);
+                                    }
+                                }
+                                catch (Exception e)
+                                {
+                                    Console.WriteLine("\nOh no! An exception occurred trying to do the math.\n - Details: " + e.Message);
+                                }
+                            }
+
+                            break;
+
+                        case "2":
+                            Console.Clear();
+
+                            Console.WriteLine("Console Calculator in C# - Exponents and Radicals Operations\r");
+                            Console.WriteLine("------------------------------------------------------------\n");
+
+                            Console.WriteLine("Choose an math operation from the following list:\n");
+
+                            Console.WriteLine("\t1 - Square Root");
+                            Console.WriteLine("\t2 - Power of two");
+                            Console.WriteLine("\t3 - Exponentiation");
+
+                            Console.WriteLine($"\nNumber of times calculator was used: {numberOfOperations}");
+
+                            Console.Write("\nWhat's your option? ");
+
+                            mathOperation = Console.ReadLine();
+
+                            if (mathOperation == null || !Regex.IsMatch(mathOperation.Trim(), "^[1-3]$"))
+                            {
+                                Console.WriteLine("\nError: Unrecognized math category input.");
+                            }
+                            else
+                            {
+                                double cleanNum1 = GetValue();
+
+                                double result = 0;
+
+                                numberOfOperations++;
+
+                                try
+                                {
+                                    result = calculator.ExponentsAndRadicals(cleanNum1, mathOperation);
+
+                                    if (double.IsNaN(result))
+                                    {
+                                        Console.WriteLine("This operation will result in a mathematical error.\n");
+                                    }
+                                    else
+                                    {
+                                        Console.WriteLine("\nYour result: {0:0.##}", result);
+                                    }
+                                }
+                                catch (Exception e)
+                                {
+                                    Console.WriteLine("\nOh no! An exception occurred trying to do the math.\n - Details: " + e.Message);
+                                }
+                            }
+
+                            break;
+
+                        case "3":
+                            Console.Clear();
+
+                            Console.WriteLine("Console Calculator in C# - Trigonometric Functions\r");
+                            Console.WriteLine("--------------------------------------------------\n");
+
+                            Console.WriteLine("Choose an math operation from the following list:\n");
+
+                            Console.WriteLine("\t1 - Sine");
+                            Console.WriteLine("\t2 - Cosine");
+                            Console.WriteLine("\t3 - Tangent");
+                            Console.WriteLine("\t4 - Consecant");
+                            Console.WriteLine("\t5 - Secant");
+                            Console.WriteLine("\t6 - Cotangent");
+
+                            Console.WriteLine($"\nNumber of times calculator was used: {numberOfOperations}");
+
+                            Console.Write("\nWhat's your option? ");
+
+                            mathOperation = Console.ReadLine();
+
+                            if (mathOperation == null || !Regex.IsMatch(mathOperation.Trim(), "^[1-6]$"))
+                            {
+                                Console.WriteLine("\nError: Unrecognized math category input.");
+                            }
+                            else
+                            {
+                                double cleanNum1 = GetValue();
+
+                                double result = 0;
+
+                                numberOfOperations++;
+
+                                try
+                                {
+                                    result = calculator.TrigonometricFunctions(cleanNum1, mathOperation);
+                                    if (double.IsNaN(result))
+                                    {
+                                        Console.WriteLine("This operation will result in a mathematical error.\n");
+                                    }
+                                    else
+                                    {
+                                        Console.WriteLine("\nYour result: {0:0.##}", result);
+                                    }
+                                }
+                                catch (Exception e)
+                                {
+                                    Console.WriteLine("\nOh no! An exception occurred trying to do the math.\n - Details: " + e.Message);
+                                }
+                            }
+
+                            break;
+
+                        default:
+                            break;
                     }
                 }
-                Console.WriteLine("------------------------\n");
+
+                Console.WriteLine("\n------------------------\n");
 
                 // Wait for the user to respond before closing.
                 Console.Write("Press 'n' and Enter to close the app, or press any other key and Enter to continue: ");
                 if (Console.ReadLine() == "n")
                 {
-                    endApp = true;
+                    isEndApp = true;
                 }
 
-                Console.WriteLine("\n"); // Friendly linespacing.
+                Console.Clear();
             }
+
             calculator.Finish();
 
             return;
+        }
+
+        public static double GetValue()
+        {
+            Console.Write("\nType a number, and then press Enter: ");
+            string? userInput = Console.ReadLine();
+
+            double userNumber = 0;
+
+            while (!double.TryParse(userInput, out userNumber))
+            {
+                Console.Write("This is not valid input. Please enter a numeric value: ");
+                userInput = Console.ReadLine();
+            }
+
+            return userNumber;
         }
     }
 }
--- AlexRamos.Calculator/CalculatorLibrary/CalculatorLibrary.cs
+++ AlexRamos.Calculator/CalculatorLibrary/CalculatorLibrary.cs
@@ -22,55 +22,133 @@ namespace CalculatorLibrary
             writer.WriteStartArray();
         }
 
-        /*
-        public Calculator()
-        {
-            StreamWriter logFile = File.CreateText("calculator.log");
-            Trace.Listeners.Add(new TextWriterTraceListener(logFile));
-            Trace.AutoFlush = true;
-            Trace.WriteLine("Starting Calcultator Log");
-            Trace.WriteLine(String.Format("Started {0}", System.DateTime.Now.ToString()));
-        }
-        */
-
-        public double DoOperation(double numberOne, double numberTwo, string operation)
+        public double BasicArithmeticOperations(double numberOne, double numberTwo, string operation)
         {
             double result = double.NaN; // Default value is "not-a-number" if an operation, such as division, could result in an error.
 
             writer.WriteStartObject();
-            writer.WritePropertyName("Operand1");
+            writer.WritePropertyName("Operand one");
             writer.WriteValue(numberOne);
-            writer.WritePropertyName("Operand2");
+            writer.WritePropertyName("Operand two");
             writer.WriteValue(numberTwo);
             writer.WritePropertyName("Operation");
 
             // Use a switch statement to do the math.
             switch (operation)
             {
-                case "a":
+                case "1":
                     result = numberOne + numberTwo;
-                    //Trace.WriteLine(String.Format("{0} + {1} = {2}", numberOne, numberTwo, result));
                     writer.WriteValue("Add");
                     break;
-                case "s":
+
+                case "2":
                     result = numberOne - numberTwo;
-                    //Trace.WriteLine(String.Format("{0} - {1} = {2}", num1, num2, result));
                     writer.WriteValue("Subtract");
                     break;
-                case "m":
+
+                case "3":
                     result = numberOne * numberTwo;
-                    //Trace.WriteLine(String.Format("{0} * {1} = {2}", num1, num2, result));
                     writer.WriteValue("Multiply");
                     break;
-                case "d":
+
+                case "4":
                     // Ask the user to enter a non-zero divisor.
                     if (numberTwo != 0)
                     {
                         result = numberOne / numberTwo;
-                        //Trace.WriteLine(String.Format("{0} / {1} = {2}", num1, num2, result));
                     }
                     writer.WriteValue("Divide");
                     break;
+
+                // Return text for an incorrect option entry.
+                default:
+                    break;
+            }
+            writer.WritePropertyName("Result");
+            writer.WriteValue(result);
+            writer.WriteEndObject();
+
+            return result;
+        }
+
+        public double ExponentsAndRadicals(double userNumber, string operation)
+        {
+            double result = double.NaN;
+
+            writer.WriteStartObject();
+            writer.WritePropertyName("Operand one");
+            writer.WriteValue(userNumber);
+            writer.WritePropertyName("Operation");
+
+            switch (operation)
+            {
+                case "1":
+                    result = Math.Sqrt(userNumber);
+                    writer.WriteValue("Square root");
+                    break;
+
+
+                case "2":
+                    result = Math.Pow(userNumber, 2);
+                    writer.WriteValue("Power of two");
+                    break;
+
+                case "3":
+                    result = Math.Pow(10, userNumber);
+                    writer.WriteValue("Exponentiation");
+                    break;
+
+                default:
+                    break;
+            }
+            writer.WritePropertyName("Result");
+            writer.WriteValue(result);
+            writer.WriteEndObject();
+
+            return result;
+        }
+
+        public double TrigonometricFunctions(double userNumber, string operation)
+        {
+            double result = double.NaN;
+
+            writer.WriteStartObject();
+            writer.WritePropertyName("Operand one");
+            writer.WriteValue(userNumber);
+            writer.WritePropertyName("Operation");
+
+            switch (operation)
+            {
+                case "1":
+                    result = Math.Sin((userNumber * Math.PI / 180));
+                    writer.WriteValue("Sine");
+                    break;
+
+                case "2":
+                    result = Math.Cos((userNumber * Math.PI / 180));
+                    writer.WriteValue("Cosine");
+                    break;
+
+                case "3":
+                    result = Math.Tan((userNumber * Math.PI / 180));
+                    writer.WriteValue("Tangent");
+                    break;
+
+                case "4":
+                    result = 1 / Math.Sin((userNumber * Math.PI / 180));
+                    writer.WriteValue("Consecant");
+                    break;
+
+                case "5":
+                    result = 1 / Math.Cos((userNumber * Math.PI / 180));
+                    writer.WriteValue("Secant");
+                    break;
+
+                case "6":
+                    result = 1 / Math.Tan((userNumber * Math.PI / 180));
+                    writer.WriteValue("Cotangent");
+                    break;
+
                 // Return text for an incorrect option entry.
                 default:
                     break;
